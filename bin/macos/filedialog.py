608255780b41156e80fe6d0241b493c81aa5d2c2
Gonen <gonengazit@gmail.com>
2021-10-17 23:13:13 +0300

    better/more functional mac file dialog

--- bin/macos/filedialog.py
+++ bin/macos/filedialog.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import tkinter as tk
 from tkinter import filedialog
 import argparse
@@ -7,12 +8,15 @@ def main():
     args=parser.parse_args()
     root = tk.Tk()
     root.withdraw()
+    root.lift()
+    root.wm_attributes("-topmost",True)
+    root.after_idle(root.call, 'wm', 'attributes', '.', '-topmost', False)
 
     if args.type == "open":
         file_path = filedialog.askopenfilename(filetypes=[("pico-8 cart", "*.p8")])
     else:
         file_path = filedialog.asksaveasfilename(defaultextension='.p8',filetypes=[("pico-8 cart", "*.p8")])
-    if file_path is None:
+    if not file_path:
         print(0,"no path")
     else:
         print(1,file_path)
